397ad894414f31cac693322d9f4ee4469ace1931
NgoMinhToan <mtoan2610@gmail.com>
2023-03-21 09:57:45 +0700

    update

--- src/context/NewsContext.js
+++ src/context/NewsContext.js
@@ -3,7 +3,7 @@ import { createContext, useReducer } from "react";
 import axios from 'axios'
 
 const HOST = process.env.REACT_APP_IN_DOCKER && process.env.REACT_APP_IN_DOCKER==1 
-    ? window.location.host.replace('3000', '8000') 
+    ? `http://${window.location.host}`.replace('3000', '8000') 
     : process.env.REACT_APP_HOST_NAME_DOCKER || process.env.REACT_APP_HOST_NAME
 
 const NewsContext = createContext();
